1a4bf7343754c20c980c19f16b6c2ea8c0b6c73b
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-06-18 13:04:18 -0400

    linking hotpants.js

---
+++ assets/js/hotpants.js
@@ -0,0 +1,30 @@
+function guardian(){
+
+		var politician = $('#politician option:selected').val();
+		var issue = $('#issue option:selected').val();
+    var queryURL = "http://content.guardianapis.com/search?section=us-news&q=" + politician + "%20AND%20" + issue + "&limit=3&api-key=2483b33c-e60d-4d3c-930f-d26c87e3c6aa";
+
+	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			console.log(queryURL);
+    	console.log(response);
+
+	});
+}
+
+
+$( document ).ready(function() {
+
+//Get politcian and subject from dropdown for Guardian API
+	$('#submit').on('click', function(){
+		
+		var politician = $('#politician option:selected').val();
+		var issue = $('#issue option:selected').val();
+	console.log(politician);
+	console.log(issue);
+		guardian();
+		return false;
+
+
+	});
+
+});
