24385978eacece3344827e469e05a52d17ac5c95
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-06-21 21:19:47 -0400

    politifact api responding

--- assets/js/hotpants.js
+++ assets/js/hotpants.js
@@ -10,14 +10,14 @@ function guardian(){
 
     //Call the Guardian API
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-			console.log(queryURL);
+			// console.log(queryURL);
     	console.log(response);
 
     $("#guardianDisplay").append("<h2>Latest News from " + "<img src='assets/images/guardian.gif' alt=guardianLogo>" + "</h2>")
 
 	//Format the information to display 3 articles on the page with links to the Guardian
 	for (var i = 0; i < 3; i++) { 
-    	var g = $("<div>");
+    	//var g = $("<div>");
 		$("#guardianDisplay").append("<a href='" + response.response.results[i].webUrl + "' target='_blank''>" + "<h3>" + response.response.results[i].webTitle + "</h3>" + "</a>");
 
     	}
@@ -25,6 +25,34 @@ function guardian(){
 	});
 }
 
+function politifact(){
+		//Clear politfact div
+	$("#politifactDisplay").html("");
+	
+	//Get politician and subject from dropdown for Politifact API
+		var politician = $('#politician option:selected').val();
+		var issue = $('#issue option:selected').val();
+    var queryURL = "https://crossorigin.me/http://www.politifact.com/api/v/2/statement/?speaker__name_slug=" + politician + "&subject__subject_slug=" + issue +"&ordering=-ruling_date&limit=3&format=json";
+    //var queryURL = "https://crossorigin.me/http://www.politifact.com/api/v/2/statement/?speaker__name_slug=bernie-s&subject__subject_slug=campaign-finance&ordering=-ruling_date&limit=3&format=json";
+
+    //Call the Guardian API
+	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			console.log(queryURL);
+    	console.log(response);
+
+    $("#politifactDisplay").append("hello")
+
+	//Format the information to display 3 articles on the page with links to the Guardian
+	for (var i = 0; i < 3; i++) { 
+    	var g = $("<div>");
+		$("#politifactDisplay").append("hello again");
+
+    	}
+
+	});
+
+
+}
 
 $( document ).ready(function() {
 
@@ -36,6 +64,7 @@ $( document ).ready(function() {
 	console.log(politician);
 	console.log(issue);
 		guardian();
+		politifact();
 		return false;
 
 
