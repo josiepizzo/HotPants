ba5bc71f908fd60bc05a2f879dd445f02841f142
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-06-22 23:43:29 -0400

    completed api build

--- assets/js/hotpants.js
+++ assets/js/hotpants.js
@@ -13,11 +13,10 @@ function guardian(){
 			// console.log(queryURL);
     	console.log(response);
 
-    $("#guardianDisplay").append("<h2>Latest News from " + "<img src='assets/images/guardian.gif' alt=guardianLogo>" + "</h2>")
+    $("#guardianDisplay").append("<h2>Latest News from " + "<img src='assets/images/guardian.gif' alt='guardianLogo'>" + "</h2>")
 
 	//Format the information to display 3 articles on the page with links to the Guardian
 	for (var i = 0; i < 3; i++) { 
-    	//var g = $("<div>");
 		$("#guardianDisplay").append("<a href='" + response.response.results[i].webUrl + "' target='_blank''>" + "<h3>" + response.response.results[i].webTitle + "</h3>" + "</a>");
 
     	}
@@ -33,26 +32,50 @@ function politifact(){
 		var politician = $('#politician option:selected').val();
 		var issue = $('#issue option:selected').val();
     var queryURL = "https://crossorigin.me/http://www.politifact.com/api/v/2/statement/?speaker__name_slug=" + politician + "&subject__subject_slug=" + issue +"&ordering=-ruling_date&limit=3&format=json";
-    //var queryURL = "https://crossorigin.me/http://www.politifact.com/api/v/2/statement/?speaker__name_slug=bernie-s&subject__subject_slug=campaign-finance&ordering=-ruling_date&limit=3&format=json";
+    
 
     //Call the Guardian API
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 			console.log(queryURL);
     	console.log(response);
 
-    $("#politifactDisplay").append("hello")
-
-	//Format the information to display 3 articles on the page with links to the Guardian
+	//Format the information to display 3 articles on the page with ratings from  Politifact
 	for (var i = 0; i < 3; i++) { 
-    	var g = $("<div>");
-		$("#politifactDisplay").append("hello again");
+		$("#politifactDisplay").append("<div class='col-md-12 panel panel-default pull-left'> <div class='panel-body'><div class='col-md-4'><img src='" + response.objects[i].speaker.photo + "' alt=" + politician + " width='84' height='73'> </div><div class='col-md-4'> <a href='" + response.objects[i].canonical_url + "'>"+ response.objects[i].statement + "</a><br> -- "+ response.objects[i].speaker.first_name + " " + response.objects[i].speaker.last_name + "</div><div class='col-md-4 pull-right'><img src='" + response.objects[i].ruling.ruling_graphic + "' alt=" + response.objects[i].ruling.ruling + " width='84' height='73'> </div></div></div>");
+   }
 
-    	}
+	// Alternate Format 
+	for (var i = 0; i < 3; i++) { 
+		$("#politifactDisplay").append("<div class='col-md-12 panel panel-default pull-left'> <div class='panel-body'><ul class='media-list'><li class='media'><div class='media-left'><a href='" + response.objects[i].canonical_url + "'><img class='media-object' src='" + response.objects[i].speaker.photo + "' alt=" + politician + " width='84' height='73'><img class='media-object' src='" + response.objects[i].ruling.ruling_graphic + "' alt=" + response.objects[i].ruling.ruling + " width='84' height='73'></div><div class='media-body'><h4 class='media-heading'>"  + response.objects[i].statement + "</h4> -- " + response.objects[i].speaker.first_name + " " + response.objects[i].speaker.last_name + " </a></div>  </li></ul>");
+	}
 
 	});
-
-
 }
+
+function giphy() {
+
+		$('#giphyDisplay').empty();
+		var politician = $('#politician option:selected').val();
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + politician + "&limit=1&api_key=dc6zaTOxFJmzC";
+		
+		// Creates AJAX call for the button that was clicked
+		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			console.log(response);
+
+			//loop through response array
+			for (var i = 0; i < response.data.length; i++) {
+				response.data[i]
+
+				var poliGif = $('<img>').attr("src", response.data[i].images.fixed_height.url);
+
+			// loads image to the page
+			$('#giphyDisplay').append(poliGif);
+		};
+
+		});
+
+	}
+
 
 $( document ).ready(function() {
 
@@ -65,6 +88,7 @@ $( document ).ready(function() {
 	console.log(issue);
 		guardian();
 		politifact();
+		giphy();
 		return false;
 
 
